2e699f0dba57c816a5e15e96a64c3fef92ffec96
serdar-ozata <serdar1901@gmail.com>
2024-10-14 23:54:39 +0300

    formatting

--- NeuralNet/optimizer.c
+++ NeuralNet/optimizer.c
@@ -6,6 +6,8 @@
 
 void adam_step(Matrix *gradients, Matrix *gradients_bias, gcnLayer *layer, double lr, double beta1, double beta2,
                double epsilon, int t) {
+    double beta1_corr = 1 - pow(beta1, t + 1);
+    double beta2_corr = 1 - pow(beta2, t + 1);
     for (int i = 0; i < gradients->m; i++) {
         for (int j = 0; j < gradients->n; j++) {
             // first and second moments for weights
@@ -14,8 +16,8 @@ void adam_step(Matrix *gradients, Matrix *gradients_bias, gcnLayer *layer, doubl
             double grad_sqr = gradients->entries[i][j] * gradients->entries[i][j];
             layer->v_weights->entries[i][j] = beta2 * layer->v_weights->entries[i][j] +
                                               (1 - beta2) * grad_sqr;
-            double mHat = layer->m_weights->entries[i][j] / (1 - pow(beta1, t + 1));
-            double vHat = layer->v_weights->entries[i][j] / (1 - pow(beta2, t + 1));
+            double mHat = layer->m_weights->entries[i][j] / beta1_corr;
+            double vHat = layer->v_weights->entries[i][j] / beta2_corr;
             layer->weights->entries[i][j] -= lr * mHat / (sqrt(vHat) + epsilon);
         }
     }
@@ -25,8 +27,8 @@ void adam_step(Matrix *gradients, Matrix *gradients_bias, gcnLayer *layer, doubl
         layer->m_bias[j] = beta1 * layer->m_bias[j] + (1 - beta1) * gradients_bias->entries[0][j];
         double grad_sqr = gradients_bias->entries[0][j] * gradients_bias->entries[0][j];
         layer->v_bias[j] = beta2 * layer->v_bias[j] + (1 - beta2) * grad_sqr;
-        double mHat = layer->m_bias[j] / (1 - pow(beta1, t + 1));
-        double vHat = layer->v_bias[j] / (1 - pow(beta2, t + 1));
+        double mHat = layer->m_bias[j] / beta1_corr;
+        double vHat = layer->v_bias[j] / beta2_corr;
         layer->bias[j] -= lr * mHat / (sqrt(vHat) + epsilon);
     }
 }
